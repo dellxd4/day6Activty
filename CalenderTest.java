01fb32bb730c79949de1c812cf2c96765df6e522
Rohan Kudalkar <rohan.kudalkar1@gmail.com>
2022-02-14 17:38:41 +0530

    unit test for calender

---
+++ CalenderTest.java
@@ -0,0 +1,8 @@
+public class CalenderTest{
+    public static void main(String[] args) {
+        System.out.println("Using unit testing");
+        System.out.println("Creating instance of CalenderTest");
+        System.out.println("*******************");
+        System.out.println("*******************");
+    }
+}
